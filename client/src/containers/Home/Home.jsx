887e2a18c0803bd330607d9bf3bbd5a5ba48f673
Joseph Perry <josephperry720@gmail.com>
2020-12-03 12:50:55 -0500

    added project section

--- client/src/containers/Home/Home.jsx
+++ client/src/containers/Home/Home.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import About from "../../components/About/About";
 import "./Home.css";
+import ProjectCard from "../../components/ProjectCard/ProjectCard";
+import projects from "../../json/projects.json";
 
 const Home = () => {
   return (
@@ -10,6 +12,13 @@ const Home = () => {
         <div className="col s12">
           <About />
         </div>
+      </div>
+      <div className="row">
+      <h3 className="center" id="projects">Projects</h3>
+        {/* maps over json array of project details and images*/}
+        {projects.map((project) => (
+          <ProjectCard key={project.id} {...project} />
+        ))}
       </div>
     </div>
   );
